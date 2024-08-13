101ccc24099a3eb22c139f7074618db22246b192
Diba Mirza <dimirza@cs.ucsb.edu>
2024-08-13 08:31:37 -0700

    Update lect5.py

--- lect5/lect5.py
+++ lect5/lect5.py
@@ -1,10 +1,6 @@
 # Lecture 5 Topics (zybook 2.11 - 2.13 )
 # Print vs. return, developing code iteratively with functions, boolean expressions and logical operators
 # Python Tutor: https://pythontutor.com/
-# Print vs. return
-
-
-
 
 # Problem 1: write a function that checks if a number is even
 def isEven(num):
