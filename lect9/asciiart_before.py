76e8f5972f634b3f438aa9cb6e13275f77e30b91
Diba Mirza <dimirza@cs.ucsb.edu>
2024-08-20 19:02:23 -0700

    lect9

---
+++ lect9/asciiart_before.py
@@ -0,0 +1,25 @@
+# By the end of this lecture you should be able to complete
+# ZyLab 4.5 (nested loops)
+# ZyLabs 4.11 and 4.12 (Accumulator pattern)
+
+# Nested Loops, practice problems with ASCII Art
+# # 1. Draw a rectangle, given its width and height
+# # 2. Draw the letter C, given its width and height
+
+
+# 1. Write a function that returns a rectangle of *'s', given its width and height
+def getRectangle(width, height):
+  '''returns a string representing a 
+     rectangle of *s with given width and height'''
+  return ""
+
+
+print(getRectangle(3, 4))
+
+
+# 2. Write a function that returns a string representing the letter C, given its width and height
+def getC(width, height):
+  '''returns a string representing the letter C 
+     using *s with given width and height'''
+  pass
+
---
+++ lect9/tictactoe_before.py
@@ -0,0 +1,8 @@
+## Let's use the AI tutor duck to get started with a tic tac toe game
+
+# Prompt: I want to develop a tic tac toe game, how should I represent the board and why?
+
+# Prompt: Can you give me an example to illustrate the point?
+
+# Prompt:
+# I want to work on three functions related to tic tac toe game, one that prints the board, another that updates the board and one that determines whether a winning configuration was attained. Give me the stub of each function in python
