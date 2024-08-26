5d54760866e885412d331fa8cd377392df4cd8c1
Diba Mirza <dimirza@cs.ucsb.edu>
2024-08-25 20:07:06 -0700

    lect 10

---
+++ lect10/tictactoe_before.py
@@ -0,0 +1,9 @@
+# tic tac toe game dev
+# Lecture 10: Board representation as 2D list, modifying and printing the board 
+
+# Last lecture you explored the following questions on your own
+# Prompt: I want to develop a tic tac toe game, how should I represent the board and why?
+
+
+# Prompt:
+# I want to work on three functions related to tic tac toe game, one that prints the board, another that updates the board and one that determines whether a winning configuration was attained. Give me the stub of each function in python
