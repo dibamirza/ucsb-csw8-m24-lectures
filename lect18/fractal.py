715516527ea7ad9e432f8a2b83b76271d49458f5
dibamirza <dimirza@cs.ucsb.edu>
2024-09-10 16:08:49 -0700

    lect 18

---
+++ lect18/fractal.py
@@ -0,0 +1,17 @@
+# Basics of turtle graphics!!
+import turtle
+jane = turtle.Turtle()
+jane.shape("turtle")
+jane.width(5)
+jane.color("green")
+jane.speed(0)
+jane.left(90)
+jane.forward(100)
+jane.left(30)
+jane.forward(100)
+jane.backward(100)
+jane.right(60)
+jane.forward(100)
+jane.backward(100)
+jane.left(30)
+jane.backward(100)
